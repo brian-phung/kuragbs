decccfa19c99a324138f93c5f996bd28e76b7a26
Brian Phung <phunglesonhai.code1@gmail.com>
2020-02-08 15:42:00 +0700

    a1

---
+++ types/ActiveSpreadApp.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="google-apps-script" />
+declare type Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+declare type Spread = GoogleAppsScript.Spreadsheet.Spreadsheet;
+export declare class ActiveSpreadApp {
+    id: string | undefined;
+    spread: Spread;
+    currentSheet: Sheet | undefined;
+    constructor();
+    onGetActiveSpread(): Spread;
+    onGetSheetValues(sheetName: string, sheetRange: string): any[][];
+    onGetSheetValuesAndFormulas(sheetName: string, sheetRange: string): any[][][];
+}
+export declare var SpreadsheetApp: any;
+export {};
